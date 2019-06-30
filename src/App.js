f46090f3b6031222b44eb0865784e9f839ad1d4b
whitney227 <whitneylcrawford@gmail.com>
2019-06-30 14:04:51 -0400

    created component folders and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import React, {Component} from 'react';
+//import './App.css';
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import ImgCard from "./components/ImgCard";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
-function App() {
+class App extends Component {
+
+
+
+
+
+
   return (
     <div>
       <header className="App-header">
